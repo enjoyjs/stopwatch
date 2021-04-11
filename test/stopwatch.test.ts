3a088e153b482e11cbb824ef97bb48b7181c0f1c
Yusei Yamamoto <yuseiyamamoto.dev@outlook.com>
2021-04-12 03:10:56 +0900

    test: add lap() test

--- test/stopwatch.test.ts
+++ test/stopwatch.test.ts
@@ -1,10 +1,9 @@
 import {strict as assert} from 'assert';
 import Stopwatch from '../src';
 
-let mockReturnValue = 0;
-const mockFn = (): number => mockReturnValue;
-
-const test = (): void => {
+const basic = (): void => {
+	let mockReturnValue = 0;
+	const mockFn = (): number => mockReturnValue;
 	const sw = new Stopwatch(mockFn);
 
 	assert(!sw.isRunning());
@@ -48,4 +47,55 @@ const test = (): void => {
 	assert(sw.getTime() === 0);
 };
 
-test();
+const lap = (): void => {
+	let mockReturnValue = 0;
+	const mockFn = (): number => mockReturnValue;
+	const sw = new Stopwatch(mockFn);
+
+	assert(sw.lap() === 0);
+
+	sw.start();
+
+	assert(sw.lap() === 0);
+	mockReturnValue += 100;
+	assert(sw.lap() === 100);
+	mockReturnValue += 200;
+	assert(sw.lap() === 200);
+	mockReturnValue += 300;
+
+	sw.pause();
+
+	mockReturnValue += 400;
+
+	sw.start();
+
+	mockReturnValue += 500;
+
+	sw.pause();
+
+	mockReturnValue += 600;
+	assert(sw.lap() === 800);
+	assert(sw.lap() === 0);
+	assert(sw.lap() === 0);
+	mockReturnValue += 700;
+
+	sw.start();
+
+	assert(sw.lap() === 0);
+	mockReturnValue += 800;
+	assert(sw.lap() === 800);
+	assert(sw.lap() === 0);
+	assert(sw.lap() === 0);
+	mockReturnValue += 900;
+	assert(sw.lap() === 900);
+	mockReturnValue += 1000;
+
+	sw.pause();
+	sw.reset();
+	sw.start();
+
+	assert(sw.lap() === 0);
+};
+
+basic();
+lap();
